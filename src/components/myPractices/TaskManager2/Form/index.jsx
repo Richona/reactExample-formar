e0bbef6cb6e6ee7ff58eacf342e2bc57064b0e15
Richona <105828822+Richona@users.noreply.github.com>
2023-01-24 22:21:31 -0300

    add taskManager2

--- src/components/myPractices/TaskManager2/Form/index.jsx
+++ src/components/myPractices/TaskManager2/Form/index.jsx
@@ -1,12 +1,18 @@
-import { Form, Button } from "react-bootstrap";
-
-export const FormTask2 = ({ onChange, inputsValues, onSubmit, refForm }) => {
+import { Form, Button, Image } from "react-bootstrap";
+import classes from './style.module.css';
+export const FormTask = ({
+  onChange,
+  inputsValues,
+  onSubmit,
+  refForm,
+  action,
+  onReset
+}) => {
   return (
     <Form onSubmit={onSubmit} ref={refForm}>
       <Form.Group className="mb-3">
         <Form.Label>Titulo</Form.Label>
         <Form.Control
-          type="text"
           placeholder="Ingresar un titulo"
           value={inputsValues.title}
           onChange={onChange}
@@ -15,33 +21,38 @@ export const FormTask2 = ({ onChange, inputsValues, onSubmit, refForm }) => {
       </Form.Group>
 
       <Form.Group className="mb-3">
+
+        <Image className={classes.imgPreview} fluid src={inputsValues.img ? inputsValues.img : "https://www.freeiconspng.com/uploads/no-image-icon-13.png"}/>
+
         <Form.Label>Imagen</Form.Label>
         <Form.Control
-          type="text"
           placeholder="Ingresar un url"
-          value={inputsValues.img}
+          value={inputsValues.img ? inputsValues.img : "https://www.freeiconspng.com/uploads/no-image-icon-13.png"}
           onChange={onChange}
           name="img"
         />
       </Form.Group>
 
-
+      <Form.Group className="mb-3">
         <Form.Label>Descripción</Form.Label>
         <Form.Control
-          type="text"
-          as={"textarea"}
-          value={inputsValues.description}
+          name="description"
           placeholder="Ingresar una descripción"
           onChange={onChange}
-          name="description"
+          as={"textarea"}
+          defaultValue={inputsValues.description}
         />
+      </Form.Group>
 
-        
-      <Button variant="success" type="submit" className="mx-2">
-        Agregar
+      <Button
+        variant={action === "CREATE" ? "success" : "info"}
+        type="submit"
+        className="mx-2"
+      >
+        {action === "CREATE" ? "Crear" : "Actualizar"}
       </Button>
-      <Button variant="danger" type="reset">
-        Reiniciar
+      <Button variant="danger" type="reset" onClick={onReset}>
+        {action === "CREATE" ? "Reiniciar" : "Cancelar"}
       </Button>
     </Form>
   );
--- src/components/myPractices/TaskManager2/index.jsx
+++ src/components/myPractices/TaskManager2/index.jsx
@@ -1,16 +1,114 @@
-import { useRef } from "react";
+import { useEffect } from "react";
+import { useReducer } from "react";
+import { useRef, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { filterTask } from "../../../constants";
 import { useForm } from "../../../hook/useForm";
-import { FormTask2 } from "./Form";
+import { taskReducer } from "../../../reducers/taskReducer";
+import { CardItem } from "./CardItem";
+import { FormTask } from "./Form";
+import { TaskFilter } from "./TaskFilter";
 
 export const TaskManager2 = () => {
+  const formTaskInitialState = {
+    id: "",
+    title: "",
+    description: "",
+    img: "",
+    active: false,
+    completed: false,
+    date: "",
+  };
   const refForm = useRef(null);
-  const [inputsValues,/* setInputsValues */,handleChangeInputsValue, reset] = useForm({},refForm)
+  const [inputsValues, setInputsValues, handleChangeInputsValue, reset] =
+    useForm(formTaskInitialState, refForm);
+  const [action, setAction] = useState("CREATE");
+  const [statusFilter, setStatusFilter] = useState(filterTask.ALL);
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      reset()
+  const tasksStore = localStorage.getItem("tasks2")
+  const initialStateReducer = JSON.parse(tasksStore) || []
+  const [tasks, dispatch] = useReducer(taskReducer, initialStateReducer); // dispatch({type,payload})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (action === "CREATE") {
+      dispatch({ type: "ADD", payload: inputsValues });
+      /* 
+      {
+    id,
+    title,
+    description,
+    img,
+    active,
+    completed,
+    date,
+  } */
     }
+
+    if (action === "UPDATE") {
+      dispatch({ type: "UPDATE", payload: inputsValues });
+      /* 
+      {
+    id,
+    title,
+    description,
+    img,
+    active,
+    completed,
+    date,
+  } */
+    }
+
+    reset();
+    setAction("CREATE");
+  };
+
+  useEffect(() => {
+    console.log(tasks);
+    localStorage.setItem("tasks2", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleUpdate = (id) => {
+    // console.log("quiero actualizar el producto" + id);
+    const taskFound = tasks.find((task) => task.id === id);
+    setInputsValues(taskFound);
+    setAction("UPDATE");
+  };
+
+  const handleDelete = (id) => {
+    dispatch({ type: "DELETE", payload: id }); /* 12ggvdwvu123 */
+  };
+
+  const handleTaskActive = (id) => {
+    dispatch({ type: "TOGGLE_ACTIVE", payload: id }); /* 12ggvdwvu123 */
+  };
+
+  const handleTaskCompleted = (id) => {
+    dispatch({ type: "TOGGLE_COMPLETE", payload: id }); /* 12ggvdwvu123 */
+  };
+
+  const handleStatusFilter = (status = "") => {
+    setStatusFilter(status);
+  };
+
+  const filterTaskMethod = (task) => {
+    switch (statusFilter) {
+      case filterTask.PROCESS:
+        return task.active === true;
+      case filterTask.PENDING:
+        return task.active === false;
+      case filterTask.COMPLETED:
+        return task.completed === true;
+      default:
+        return task;
+    }
+  }
+
+  const handleReset = () => {
+    reset()
+  }
+
   /*{
       title:"titulo 1",
       img:"https://img/perritos.png",
@@ -20,15 +118,39 @@ export const TaskManager2 = () => {
   return (
     <Container className="mt-5">
       <Row>
+        <Col sm={12} lg={{ span: 6, offset: 5 }} className="mb-4">
+          <TaskFilter onChangeFilter={handleStatusFilter} />
+        </Col>
         <Col sm={12} lg={3}>
-          <FormTask2
+          <FormTask
             onChange={handleChangeInputsValue}
-            inputsValues={inputsValues}
             onSubmit={handleSubmit}
+            inputsValues={inputsValues}
             refForm={refForm}
+            action={action}
+            onReset={handleReset}
           />
         </Col>
-        <Col sm={12} lg={9}></Col>
+        <Col
+          sm={12}
+          lg={9}
+          className="d-flex flex-wrap align-items-start gap-2"
+        >
+          {tasks
+            .filter(filterTaskMethod)
+            .map((task) => {
+              return (
+                <CardItem
+                  key={task.id}
+                  task={task}
+                  onUpdate={handleUpdate}
+                  onDelete={handleDelete}
+                  onActive={handleTaskActive}
+                  onCompleted={handleTaskCompleted}
+                />
+              );
+            })}
+        </Col>
       </Row>
     </Container>
   );
